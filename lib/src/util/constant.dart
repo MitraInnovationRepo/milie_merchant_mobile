3a7dc5fe7cdc51d80cc8c63937b9b293b89b95c9
Lakshitha Ranasinghe <lranasinghe@mitrai.com>
2020-10-16 15:58:13 +0530

    prod configuration

--- lib/src/util/constant.dart
+++ lib/src/util/constant.dart
@@ -4,23 +4,23 @@ class Constant {
   static double sriLankaLongitude = 80.7718;
 
   //Dev
-  static String clientSecret = "b44d59e3-0b25-4aee-9668-12e66ace71eb";
-  // static String backendEndpoint = "https://dev-api.milieapp.com/api";
-  static String backendEndpoint = "http://10.0.2.2:8083/api";
-  static String keycloakEndpoint = "https://dev-api.milieapp.com:8443";
-  static String contentEndpoint = "http://10.0.2.2:1337";
+  // static String clientSecret = "b44d59e3-0b25-4aee-9668-12e66ace71eb";
+  // // static String backendEndpoint = "https://dev-api.milieapp.com/api";
+  // static String backendEndpoint = "http://10.0.2.2:8083/api";
+  // static String keycloakEndpoint = "https://dev-api.milieapp.com:8443";
+
+  //Prod
+  static String clientSecret = "5c5450dc-6f10-4e47-a52a-3c9dcc4e776f";
+  static String backendEndpoint = "https://api.foodie-apps.com/api";
+  static String pageEndpoint = "https://api.foodie-apps.com";
+  static String keycloakEndpoint = "https://auth.foodie-apps.com";
+  static String contentEndpoint = "https://content.foodie-apps.com";
+
 
   static String deliveryEndpoint = "http://link.lakmobile.com/vconnect/ext/api";
   static String deliveryAPIUserName = "mithra";
   static String deliveryAPIPassword = "mithra@875w65";
 
-  //Prod
-  // static String clientSecret = "6a535787-2507-42cc-80eb-2c2eccca6af3";
-  // static String backendEndpoint = "https://api.milieapp.com/api";
-  // static String keycloakEndpoint = "https://api.milieapp.com:8443";
-
-  static String merchantRole = "merchant";
-  static String customerRole = "customer";
   static String tncCustomer = "http://app.milieapp.com/UserAgreement.pdf";
   static String tncMerchant = "http://app.milieapp.com/MerchantAgreement.pdf";
 
