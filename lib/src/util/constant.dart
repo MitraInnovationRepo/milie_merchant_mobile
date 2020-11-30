6aa70c678770266ddbc1f0ae5e057670f948de04
Lakshitha Ranasinghe <lranasinghe@mitrai.com>
2020-11-30 17:43:04 +0530

    changed to dev

--- lib/src/util/constant.dart
+++ lib/src/util/constant.dart
@@ -1,15 +1,16 @@
 class Constant {
   //Dev
-  // static String clientSecret = "b44d59e3-0b25-4aee-9668-12e66ace71eb";
-  // // static String backendEndpoint = "https://dev-api.milieapp.com/api";
+  static String clientSecret = "265c6a99-607e-4324-8bcd-23c7d8e50508";
   // static String backendEndpoint = "http://10.0.2.2:8083/api";
-  // static String keycloakEndpoint = "https://dev-api.milieapp.com:8443";
+  static String backendEndpoint = "http://ec2-65-0-73-169.ap-south-1.compute.amazonaws.com:8081/api";
+  static String contentEndpoint = "https://content.foodie-apps.com";
+  static String keycloakEndpoint = "http://ec2-65-0-73-169.ap-south-1.compute.amazonaws.com:8080";
 
   //Prod
-  static String clientSecret = "5c5450dc-6f10-4e47-a52a-3c9dcc4e776f";
-  static String backendEndpoint = "https://api.foodie-apps.com/api";
-  static String keycloakEndpoint = "https://auth.foodie-apps.com";
-  static String contentEndpoint = "https://content.foodie-apps.com";
+  // static String clientSecret = "5c5450dc-6f10-4e47-a52a-3c9dcc4e776f";
+  // static String backendEndpoint = "https://api.foodie-apps.com/api";
+  // static String keycloakEndpoint = "https://auth.foodie-apps.com";
+  // static String contentEndpoint = "https://content.foodie-apps.com";
 
 
   static String deliveryEndpoint = "http://youcab-apps.com/vconnect/ext/api";
@@ -23,7 +24,7 @@ class Constant {
 
   static String orderPrefix = "YCF0";
 
-  static String androidAppVersion = "1.1.0";
+  static String androidAppVersion = "1.0.0";
   static String iosAppVersion = "1.0.0";
 
   static String filePath = backendEndpoint + "/file/download/";
