f312b3a475008c8df5474096cc227927bb81d505
Pamali Chathurika <PChathurika@iedigital.com>
2021-01-20 11:47:16 +0530

    Released merchant app 1.2.1

--- lib/src/util/constant.dart
+++ lib/src/util/constant.dart
@@ -1,16 +1,16 @@
 class Constant {
   //Dev
-  static String clientSecret = "265c6a99-607e-4324-8bcd-23c7d8e50508";
-  // static String backendEndpoint = "http://10.0.2.2:8083/api";
-  static String backendEndpoint = "http://ec2-65-0-173-201.ap-south-1.compute.amazonaws.com:8081/api";
-  static String contentEndpoint = "https://content.foodie-apps.com";
-  static String keycloakEndpoint = "http://ec2-65-0-173-201.ap-south-1.compute.amazonaws.com:8080";
+  // static String clientSecret = "265c6a99-607e-4324-8bcd-23c7d8e50508";
+  // // static String backendEndpoint = "http://10.0.2.2:8083/api";
+  // static String backendEndpoint = "http://ec2-65-0-173-201.ap-south-1.compute.amazonaws.com:8081/api";
+  // static String contentEndpoint = "https://content.foodie-apps.com";
+  // static String keycloakEndpoint = "http://ec2-65-0-173-201.ap-south-1.compute.amazonaws.com:8080";
 
   //Prod
-  // static String clientSecret = "5c5450dc-6f10-4e47-a52a-3c9dcc4e776f";
-  // static String backendEndpoint = "https://api.foodie-apps.com/api";
-  // static String keycloakEndpoint = "https://auth.foodie-apps.com";
-  // static String contentEndpoint = "https://content.foodie-apps.com";
+  static String clientSecret = "5c5450dc-6f10-4e47-a52a-3c9dcc4e776f";
+  static String backendEndpoint = "https://api.foodie-apps.com/api";
+  static String keycloakEndpoint = "https://auth.foodie-apps.com";
+  static String contentEndpoint = "https://content.foodie-apps.com";
 
 
   static String deliveryEndpoint = "http://youcab-apps.com/vconnect/ext/api";
@@ -25,12 +25,12 @@ class Constant {
   static String orderPrefix = "YCF0";
 
   //Dev
-  static String androidAppVersion = "1.1.0";
-  static String iosAppVersion = "1.0.0";
+  // static String androidAppVersion = "1.1.0";
+  // static String iosAppVersion = "1.0.0";
 
   //Prod
-  // static String androidAppVersion = "1.2.0";
-  // static String iosAppVersion = "1.2.0";
+  static String androidAppVersion = "1.2.1";
+  static String iosAppVersion = "1.2.0";
 
   static String filePath = backendEndpoint + "/file/download/";
 }
