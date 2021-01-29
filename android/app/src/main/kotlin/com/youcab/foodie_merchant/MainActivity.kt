c4f0793ea69d5d9153b9f9c71180d0ae2fd3dc7e
thilina warnakulasuriya <thilina9292.contact@gmail.com>
2021-01-29 22:21:49 +0530

    removing toast msg

--- android/app/src/main/kotlin/com/youcab/foodie_merchant/MainActivity.kt
+++ android/app/src/main/kotlin/com/youcab/foodie_merchant/MainActivity.kt
@@ -2,7 +2,6 @@ package com.youcab.foodie_merchant
 
 import io.flutter.embedding.android.FlutterActivity
 import android.view.KeyEvent
-import android.widget.Toast
 
 class MainActivity: FlutterActivity() {
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
