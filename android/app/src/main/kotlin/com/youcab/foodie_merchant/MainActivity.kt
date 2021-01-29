fb9cbad99a51f45c6df43de2f5586b2ba4fe9dc9
thilina warnakulasuriya <thilina9292.contact@gmail.com>
2021-01-29 22:20:22 +0530

    Volume key handle

--- android/app/src/main/kotlin/com/youcab/foodie_merchant/MainActivity.kt
+++ android/app/src/main/kotlin/com/youcab/foodie_merchant/MainActivity.kt
@@ -2,9 +2,14 @@ package com.youcab.foodie_merchant
 
 import io.flutter.embedding.android.FlutterActivity
 import android.view.KeyEvent
+import android.widget.Toast
 
 class MainActivity: FlutterActivity() {
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return true
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
+                || keyCode == KeyEvent.KEYCODE_VOLUME_UP ){
+            return true
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
